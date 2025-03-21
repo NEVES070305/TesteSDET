65af3e26e20f12e076bbe00cfdbff1d3558de53a
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2025-03-21 16:27:33 -0300

    chore: modified selector added selectors of inventory

--- utils/selectors.js
+++ utils/selectors.js
@@ -3,5 +3,10 @@ module.exports = {
   passwordInput: '#password.input_error.form_input',
   loginButton: '#login-button.submit-button.btn_action',
   homePageTitle: 'Swag Labs',
-  errorMessage:  'h3[data-test="error"]'
+  errorMessage:  'h3[data-test="error"]',
+
+  //Seletores de ordenação
+  sortDropdown: 'select[data-test="product-sort-container"]',
+  productName: '.inventory_item_name',
+  productPrice: '.inventory_item_price'
 };
