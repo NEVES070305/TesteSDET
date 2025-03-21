7b09691e93b67da395c48ef8785001c2d2e30409
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2025-03-21 16:21:57 -0300

    test: added utils

---
+++ utils/users.js
@@ -0,0 +1,18 @@
+// utils/users.js
+module.exports = [
+  {
+    username: 'usuario1',
+    password: 'senha1',
+    valid: false
+  },
+  {
+    username: 'usuario2',
+    password: 'senha2',
+    valid: false
+  },
+  {
+    username: 'standard_user',
+    password: 'secret_sauce',
+    valid: true
+  }
+];
