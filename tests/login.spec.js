afa041d38d6e9586e98701fc30b711fd020ed520
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2025-03-21 16:19:15 -0300

    test: created test action and file for login

---
+++ tests/login.spec.js
@@ -0,0 +1,27 @@
+// tests/login.spec.js
+const { chromium, test, beforeAll } = require("@playwright/test");
+const { navigateToLoginPage } = require("../actions/navigationAction");
+const { login, verifyLoginSuccess, verifyLoginFailure } = require("../actions/loginAction");
+const selectors = require("../utils/selectors");
+const users = require("../utils/users");
+
+const URL =  'https://www.saucedemo.com/';
+
+users.forEach((user) => {
+  if (user.valid) {
+    test(`Login com o usuário válido: ${user.username} , esperando ser logado`, async ({ page }) => {
+      await navigateToLoginPage(page);
+      await login(page, user.username, user.password);
+      await verifyLoginSuccess(page, selectors.homePageTitle);    
+    })
+  }
+  else 
+  {
+    test(`Login com o usuário inválido: ${user.username} , esperando não ser logado`, async ({ page }) => {
+      await navigateToLoginPage(page);
+      await login(page, user.username, user.password);
+      // Espera aparecer a mensagem de erro
+      await verifyLoginFailure(page, selectors.errorMessage);
+    })
+  }
+});
