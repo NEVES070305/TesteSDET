95c6885f33cc427040aad90ff6ac772acf59d49c
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2025-03-22 22:36:04 -0300

    test: added checkout to buy products

---
+++ actions/checkoutNavigationAction.js
@@ -0,0 +1,16 @@
+const { expect } = require('@playwright/test');
+const selectors = require('../utils/selectors');
+
+async function goToCheckoutStepOne(page) {
+
+  await page.click(selectors.cartLink);
+
+  await expect(page).toHaveURL(/cart\.html$/);
+  
+  await page.click(selectors.checkoutButton);
+  
+  // Verifica que a URL corresponde à página de checkout-step-one
+  await expect(page).toHaveURL(/checkout-step-one\.html$/);
+}
+
+module.exports = { goToCheckoutStepOne };
